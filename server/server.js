2ae57bbacb6a97fe1dc9471f6fe0768c32af7cde
donnie <donnie.darker101@gmail.com>
2019-01-03 18:47:14 +0100

    added creat todo api

--- server/server.js
+++ server/server.js
@@ -1,16 +1,28 @@
 require('./db/mongoose');
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const {User} = require('./models/user');
 const {Todo} = require('./models/todo');
+
 const port = process.env.PORT || 8080;
 
 const app = express();
+app.use(bodyParser.json())
 
-app.get('/',(req,res)=>{
-    res.send('Welcome')
+app.post('/todos',(req,res)=>{
+    let {text} = req.body;
+    let todo = new Todo({
+        text
+    });
+
+    todo.save().then((savedTodo)=>{
+        res.status(200).send(savedTodo)
+    }).catch((error)=>{
+        res.status(400).send(error)
+    })
 });
-
 app.listen(port,()=>{
     console.log('The server is live')
-})
+});
 
